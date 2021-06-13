e2d31ce8edd277a08cd3e176c08f71ec31afd859
Sriram044 <85817246+Sriram044@users.noreply.github.com>
2021-06-13 18:38:13 +0530

    Add files via upload

---
+++ Day1 challenge.py
@@ -0,0 +1,35 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+# assignment 1
+# print hello World
+
+print('Hello World')
+print("by Sriram D")
+
+#add two numbers
+a=10
+b=25
+c=a+b
+print(c)
+
+# find maximum
+
+a=int(input("enter numb1: "))
+b=int(input("enter numb2: "))
+if(a>b):
+    print("num1 is maximum")
+else:
+    print("num2 is maximum")
+
+#  OR 
+
+a=int(input("enter num1:"))
+b=int(input("enter num2:"))
+c=max(a,b)
+print("maximum value is:")
+print(c)
