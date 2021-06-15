805d523de7337108296ad9814f601bdfb26b574c
Sriram044 <85817246+Sriram044@users.noreply.github.com>
2021-06-15 15:17:33 +0530

    Add files via upload

---
+++ day 4  challenge.py
@@ -0,0 +1,33 @@
+#1)Create three variables (a,b,c) to same value of any integer & do the following
+a) Divide a by 10
+b) Multiply b by 50
+c) Add c value by 60
+
+
+a=b=c=10
+div=a/10
+mul=b*50
+add=c+60
+print(div)
+print(mul)
+print(add)
+
+
+
+#2)Create a String variable of 5 characters and replace the 3rd character with G
+
+string= 'mango'
+print(string.replace('mango','maggo'))
+
+
+#3)Create two values (a,b) of int,float data type & convert the vise versa, Hint : convert a from int to float datatype & b from float to int datatype
+
+a=55
+b=55.5
+c=float(a)
+d=int(b)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
